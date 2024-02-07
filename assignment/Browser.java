a6b960523c0e2f7350463ca6a5752bd03e2bf6e1
Kruthiga-L <159278024+Kruthiga-L@users.noreply.github.com>
2024-02-07 22:12:58 +0530

    Add files via upload
    week1

---
+++ assignment/Browser.java
@@ -0,0 +1,16 @@
+package week1.day1.assignment;
+
+public class Browser {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		float browserVersion = 12.31F;
+		String browserName = "FireFox";
+		boolean pageLoaded = true;
+		int noOfBrowser = 1;
+		char browserLogo='F';
+		System.out.println("Browser Version:"+browserVersion+"\n"+"Browser Name:"+browserName+"\n"+"PageLoaded:"+pageLoaded+"\n"+
+		"No of Browser:"+noOfBrowser+"\n"+"Browser Logo:"+browserLogo);
+	}
+
+}
---
+++ assignment/FindNumber.java
@@ -0,0 +1,18 @@
+package week1.day1.assignment;
+
+public class FindNumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int value=45;
+		if (value>0) {
+			System.out.println("The number is positive");
+		}
+		else
+		{
+			System.out.println("The number is negative");
+		}
+	}
+
+}
+
